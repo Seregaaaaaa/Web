9e72dc0d3d55b95b67a3e38132133f7c3e99b798
Sergei Samoilov <89564156+Seregaaaaaa@users.noreply.github.com>
2024-12-01 22:50:41 +0700

    11

--- 932201.Samoilov.Sergei.Web.Lab11-main/MathOperationsApp/Models/MathViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MathOperationsApp.Models
-{
-    public class MathViewModel
-    {
-        public int Number1 { get; set; }
-        public int Number2 { get; set; }
-        public double? Result { get; set; }
-    }
-}
---
+++ 932201.Samoilov.Sergei.Web.Lab11-main/lab11/Program.cs
@@ -0,0 +1,36 @@
+using lab11.Interfaces;
+using lab11.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<ICalcService, CalcService>(); // add custom services
+
+// Закомментируйте или удалите следующую строку
+// builder.WebHost.ConfigureKestrel(options => options.ListenLocalhost(5001, listenOptions => listenOptions.UseHttps()));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
